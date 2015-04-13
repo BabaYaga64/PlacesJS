14d5f627012899456d2da0f22f58a42502006b04
Bojana Skarich & Bryan Borgeson <student@epicodus.com>
2015-04-13 11:50:54 -0700

    Everything works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-    $
+    $("form#new-place").submit(function(event) {
+
+        var inputtedLocation = $("input#new-location").val();
+        var inputtedNotes    = $("textarea#new-notes").val();
+        var inputtedImage    = $("input#new-image").val();
+
+
+        var newPlace = {
+                location: inputtedLocation,
+                notes:    inputtedNotes,
+                image:    inputtedImage
+            };
+
+        $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+
+        $("input#new-location").val("");
+        $("textarea#new-notes").val("");
+        $("input#new-image").val("");
+
+        $(".place").last().click(function() {
+            $("#show-place").show();
+            $("#show-place h2").text(newPlace.location);
+            $(".location").text(newPlace.location);
+            $(".notes").text(newPlace.notes);
+            $(".image").text(newPlace.image);
+        });
+
+        event.preventDefault();
+
+    });
 });
